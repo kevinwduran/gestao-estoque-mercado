4329ab86b778851d8b2f70258274aa05391a6ffc
Kevin Duran <kevinwduran23@gmail.com>
2023-10-31 21:31:50 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,181 @@
+import re
+from bson import ObjectId
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# Acessa o banco de dados
+mydb = myclient["dbMeccanotecnica"]
+# Acessa a coleção
+mycol = mydb["Products"]
+validateAccess = True
+
+@app.route("/")
+def home():
+    if(validateAccess):
+        table_data = buscarDados()
+        return render_template("index.html", table_data=table_data)
+    else:
+        return render_template("login.html")
+
+@app.route("/", methods=["POST"])
+def cadastrar():
+    nome = request.form["nome"]
+    codigo = request.form["codigo"]
+    estoqueMin = request.form["estoqueMin"]
+    categoria = request.form["categoria"]
+    preco = request.form["preco"]
+    quantidade = request.form["quantidade"]
+    unidadeMedida = request.form["unidadeMedida"]
+    status = request.form["status"]
+    
+
+    avulso = {
+        "nome": nome,
+        "codigo": codigo,
+        "estoqueMin": estoqueMin,
+        "categoria": categoria,
+        "preco": preco,
+        "quantidade": quantidade,
+        "unidadeMedida": unidadeMedida,
+        "status": status,
+    }
+    x = mycol.insert_one(avulso)
+    return redirect(url_for("home"))
+
+
+@app.route("/buscar", methods=["POST"])
+def buscaritens():
+    item = request.form["buscar-item"]
+    if item != "":
+        regex_pattern = re.compile(re.escape(item), re.IGNORECASE)
+        myquery = {"item": {"$regex": regex_pattern}}
+        mydoc = mycol.find(myquery)
+
+        results = []
+
+        for x in mydoc:
+            results.append(x)
+
+        table_data = []
+
+        for result in results:
+            table_data.append(
+                [
+                    result.get("_id"),
+                    result.get("nome", ""),
+                    result.get("codigo", ""),
+                    result.get("estoqueMin", ""),
+                    result.get("categoria", ""),
+                    result.get("preco", ""),
+                    result.get("quantidade", ""),
+                    result.get("unidadeMedida", ""),
+                    result.get("status", ""),
+                ]
+            )
+        return render_template("index.html", table_data=table_data, value_search=item)
+    else:
+        return redirect(url_for("home"))
+
+
+@app.route("/deletar/<id>", methods=["POST"])
+def deletar(id):
+    object_id = ObjectId(id)
+    myquery = {"_id": object_id}
+    result = mycol.delete_one(myquery)
+    if result.deleted_count > 0:
+        return redirect(url_for("home"))
+    else:
+        return "não tem registro"
+
+
+@app.route("/alterar/<id>", methods=["GET"])
+def alterar(id):
+    if id:
+        object_id = ObjectId(id)
+        myquery = {"_id": object_id}
+        item_data = mycol.find_one(myquery)
+
+        if item_data:
+            nome = item_data.get("nome", "")
+            codigo = item_data.get("codigo", "")
+            estoqueMin = item_data.get("estoqueMin", "")
+            categoria = item_data.get("categoria", "")
+            preco = item_data.get("preco", "")
+            quantidade = item_data.get("quantidade", "")
+            unidadeMedida = item_data.get("unidadeMedida", "")
+            status = item_data.get("status", "")
+
+            return render_template(
+                "alterItens.html",
+                id=id,
+                nome=nome,
+                codigo=codigo,
+                estoqueMin=estoqueMin,
+                categoria=categoria,
+                preco=preco,
+                quantidade=quantidade,
+                unidadeMedida=unidadeMedida,
+                status=status,
+            )
+        else:
+            return "Item não encontrado"
+    else:
+        return redirect(url_for("home"))
+
+
+@app.route("/salvarAlteracoes", methods=["POST"])
+def salvarAlteracoes():
+    id = request.form["id"]
+    nome = request.form["nome"]
+    codigo = request.form["codigo"]
+    estoqueMin = request.form["estoqueMin"]
+    categoria = request.form["categoria"]
+    preco = request.form["preco"]
+    quantidade = request.form["quantidade"]
+    unidadeMedida = request.form["unidadeMedida"]
+    status = request.form["status"]
+    object_id = ObjectId(id)
+    paraModificar = {"_id": object_id}
+    novoValor = {
+        "$set": {
+            "nome": nome,
+            "codigo": codigo,
+            "estoqueMin": estoqueMin,
+            "categoria": categoria,
+            "preco": preco,
+            "quantidade": quantidade,
+            "unidadeMedida": unidadeMedida,
+            "status": status,
+        }
+    }
+    mycol.update_one(paraModificar, novoValor)
+    return redirect(url_for("home"))
+
+
+def buscarDados():
+    all_data = mycol.find()
+
+    table_data = []
+
+    for data in all_data:
+        table_data.append(
+            [
+                data.get("_id"),
+                data.get("nome", ""),
+                data.get("codigo", ""),
+                data.get("estoqueMin", ""),
+                data.get("categoria", ""),
+                data.get("preco", ""),
+                data.get("quantidade", ""),
+                data.get("unidadeMedida", ""),
+                data.get("status", ""),
+            ]
+        )
+    return table_data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
